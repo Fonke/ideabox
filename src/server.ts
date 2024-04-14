32cc7bdea07e213c7453e02e2d5cab02beb371df
Alexis Chassagnoux <chassagnoux.alexis@gmail.com>
2024-04-14 14:45:32 +0200

    Use custom ticket id generator instead of uuid.

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,52 @@
 import https = require('https');
 import fs = require('fs');
 import ws = require('ws');
-import uuid = require('uuid');
+
+interface Ticket {
+  id: number;
+  creation_date: number;
+  title: string;
+  description: string;
+}
+
+interface Tickets {
+  accepted: Ticket[];
+  rejected: Ticket[];
+  pending: Ticket[];
+}
 
 const homePageContent = fs.readFileSync('src/index.html', 'utf-8');
 const ticketsContent = fs.readFileSync('database/tickets.json', 'utf-8');
-let tickets = JSON.parse(ticketsContent);
+let tickets: Tickets = JSON.parse(ticketsContent);
+//TODO(chassa_a): do sanity checks on tickets
+let highestTicketId: number = -1;
+
+function highestTicketIdFromDB(): number {
+  for (const ticketsArray of Object.values(tickets)) {
+    for (const ticket of ticketsArray) {
+      highestTicketId = (ticket.id > highestTicketId) ? ticket.id : highestTicketId;
+    }
+  }
+
+  return highestTicketId;
+}
+
+function createTicketId(): number {
+  //TODO(chassa_a): check if ticket id already exists
+  highestTicketId += 1;
+  return highestTicketId;
+}
 
 function moveTicket(ticketId: string, fromTicketListName: string, toTicketListName: string): void {
   if (fromTicketListName === toTicketListName) {
     return;
   }
-  const ticketIndex: number = tickets[fromTicketListName].findIndex((ticket: any): boolean => ticket.id == ticketId);
+  const ticketIndex: number = tickets[fromTicketListName as keyof Tickets].findIndex((ticket: any): boolean => ticket.id == ticketId);
   if (ticketIndex == -1) {
     throw 'ticket with id: ' + ticketId + ' not found in list ' + fromTicketListName;
   }
-  const ticket: any = tickets[fromTicketListName].splice(ticketIndex, 1).at(0);
-  tickets[toTicketListName].push(ticket);
+  const ticket: any = tickets[fromTicketListName as keyof Tickets].splice(ticketIndex, 1).at(0);
+  tickets[toTicketListName as keyof Tickets].push(ticket);
 }
 
 function onWSClientMessage(client: ws, message: string): void {
@@ -25,8 +55,8 @@ function onWSClientMessage(client: ws, message: string): void {
   if (request.name == 'get_all_tickets') {
     client.send(JSON.stringify({name: 'tickets', params: tickets}));
   } else if (request.name == 'create_ticket') {
-    const newTicket = {
-      id: uuid.v4(),
+    const newTicket: Ticket = {
+      id: createTicketId(),
       creation_date: Date.now() / 1000,
       title: request.params.ticket_title,
       description: request.params.ticket_description
@@ -48,6 +78,8 @@ function onWSClientMessage(client: ws, message: string): void {
   }
 }
 
+highestTicketId = highestTicketIdFromDB();
+console.log('highestTicketId = ' + highestTicketId);
 const cert: Buffer = fs.readFileSync('certs/server.crt');
 const certKey: Buffer = fs.readFileSync('certs/server.key');
 
