e5725d89201781eb0fcb76bf3d402f981e9d30c3
Alexis Chassagnoux <chassagnoux.alexis@gmail.com>
2024-05-01 19:54:07 +0200

    Parse cookies to retrieve auth_token.

--- src/server.ts
+++ src/server.ts
@@ -45,6 +45,15 @@ let validators: Validator[] = JSON.parse(validatorsContent);
 //TODO(chassa_a): do sanity checks on validators
 let highestTicketId: number = -1;
 
+function parseCookies(cookiesString: string): { [key: string]: string } {
+  const cookies: { [key: string]: string } = {};
+  cookiesString.split(';').forEach(cookie => {
+      const [name, value] = cookie.split('=').map(c => c.trim());
+      cookies[name] = decodeURIComponent(value);
+  });
+  return cookies;
+}
+
 function generateAuthToken(): string {
   let result: string = "";
   const randomBytes: Uint8Array= crypto.getRandomValues(new Uint8Array(32));
@@ -121,16 +130,18 @@ const certKey: Buffer = fs.readFileSync('certs/server.key');
 
 const httpsServer: https.Server = https.createServer({cert: cert, key: certKey});
 httpsServer.on("request", (request: IncomingMessage, response: ServerResponse<IncomingMessage>) => {
-  const cookiesHeader = request.headers.cookie;
-  console.log('cookiesHeader = ' + cookiesHeader);
-  if (cookiesHeader === undefined) {
-    const authToken: string = generateAuthToken();
+  let authToken: string | undefined = undefined;
+  if (request.headers.cookie !== undefined) {
+    authToken = parseCookies(request.headers.cookie).auth_token;
+  }
+  if (authToken === undefined) {
+    authToken = generateAuthToken();
     console.log('generated auth_token: ' + authToken);
     //TODO(chassa_a): add path=/, SameSite=strict, Max-Age=, etc...
     response.setHeader('Set-Cookie', `auth_token=${authToken}; Secure;`);
   }
   else {
-    console.log('client auth_token: ' + cookiesHeader);
+    console.log('client auth_token: ' + authToken);
   }
   response.writeHead(200, {'Content-Type': 'text/html'});
   response.end(homePageContent);
